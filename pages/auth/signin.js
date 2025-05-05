5b29c96dd4b7dad3a85b00b19bbc44ff1ddbd362
junaid <junaidjdsiddiqui21@gmail.com>
2025-05-05 23:48:01 +0530

    2 commit

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -19,7 +19,7 @@ export default function SignIn({ csrfToken }) {
 
         {/* ➕ Add Sign Up Link */}
         <p className="text-sm text-center mt-2">
-          Don't have an account?{" "}
+        {"Don't have an account?"}{" "}
           <Link href="/auth/signup" className="text-blue-600 hover:underline">
             Sign up
           </Link>
